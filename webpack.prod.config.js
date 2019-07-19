a69e60392d4eedc0391626a406990d4358add392
CallMeHK <hkrazy888@gmail.com>
2019-07-18 22:35:37 -0600

    initial commit

---
+++ webpack.prod.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: path.resolve(__dirname, "./src/app.tsx"),
+  devtool: "inline-source-map",
+  devServer: {
+    writeToDisk: true,
+    contentBase: path.join(__dirname, "../dist-server/app")
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.ts|\.tsx$/,
+        loader: "ts-loader",
+        options: {
+          configFile: "tsconfig.json"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  output: {
+    filename: "main.[contenthash].js",
+    path: path.resolve(__dirname, "./dist/app")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html")
+    })
+  ]
+};
